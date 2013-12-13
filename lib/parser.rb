e6754da026634f1367d77a483767c9865a16f907
Danny <dannylshaw@yahoo.com>
2013-12-13 14:02:27 -0500

    Updated parser specs

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,8 +8,9 @@ def parseHTML(html)
 	# preferably hold at least the following attributes:
 	# 1. component type (i.e image, video, URL link)
 	# 2. content (i.e image link, video link, URL)
-	# 3. if possible, can you try to include another array
+	# 3. if possible, can you try to include a hash
 	# that contains all other HTML tags in the element?
+	# something like {"class" => "body", "media" => "handheld"}
 	components = []
 
 	# parse html here, nokogiri is probably a reasonable
