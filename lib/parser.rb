bc6ffd1e57b990257fb5b7af773b732ee9821446
Danny <dannylshaw@yahoo.com>
2013-12-13 13:39:49 -0500

    Defined skeleton for parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,1 +1,19 @@
-asdf
+# yo victor i put some specs below and a basic function
+# skeleton, html will be a string holding all the HTML
+# and components will be the return array, let me know
+# if you have any questions
+
+def parseHTML(html)
+	# components will have an array of objects that each
+	# preferably hold at least the following attributes:
+	# 1. component type (i.e image, video, URL link)
+	# 2. content (i.e image link, video link, URL)
+	# 3. if possible, can you try to include another array
+	# that contains all other HTML tags in the element?
+	components = []
+
+	# parse html here, nokogiri is probably a reasonable
+	# option to try first
+
+	return components
+end
