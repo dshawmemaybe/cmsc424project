408e63eb4dcec6ede530bdc84b13a3237eb4375b
Danny <dannylshaw@yahoo.com>
2013-12-16 03:01:47 -0500

    Added opening of files from extension

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,16 @@
 //= require bootstrap-select
 //= require_tree .
 $(document).ready(function() {
-});
+  $("a").click(function() {
+    link_host = this.href.split("/")[2];
+    document_host = document.location.href.split("/")[2];
 
+    if (link_host != document_host) {
+      window.open(this.href);
+      return false;
+    }
+  });
+});
 $(function() {
 
 // Sidebar active state activation
